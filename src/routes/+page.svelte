248e22544853c833f913c1f2f5e6f4b42b882c2b
SnaveSutit <snavesutit@gmail.com>
2024-07-30 16:55:02 -0400

    🚧 Make those buttons POP!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,13 +10,6 @@
 	})
 </script>
 
-<div class="navigation">
-	<a href="/">Home</a>
-	<a href="/about">About</a>
-	<a href="/projects">Projects</a>
-	<a href="/contact">Contact</a>
-</div>
-
 <div class="container">
 	<div class="title">
 		<div>
@@ -36,37 +29,27 @@
 		</div>
 	</div>
 	<div class="card-container">
-		<div class="card">
-			<h1>Projects</h1>
-			<p>
-				Here are some of the projects I've worked on. You can find more on my{' '}
-				<a href="/projects">projects page</a>.
-			</p>
-		</div>
-		<div class="card">
-			<h1>Contact</h1>
-			<p>
-				Here are some of the projects I've worked on. You can find more on my{' '}
-				<a href="/projects">projects page</a>.
-			</p>
-		</div>
+		<button class="card">
+			<div class="img-container">
+				<img src="/img/projects_title.png" alt="Projects" draggable="false" />
+			</div>
+		</button>
+		<button class="card">
+			<div class="img-container">
+				<img src="/img/about_title.png" alt="About" draggable="false" />
+			</div>
+		</button>
+		<button class="card">
+			<div class="img-container">
+				<img src="/img/contact_title.png" alt="Contact" draggable="false" />
+			</div>
+		</button>
 	</div>
 </div>
 
 <style>
-	.navigation {
-		position: fixed;
-		top: 0;
-		left: 0;
-		width: 100%;
-		display: flex;
-		background-color: var(--color-foreground);
-		box-shadow: 0 0px 10px rgba(0, 0, 0, 1);
-	}
-	.navigation a {
-		color: var(--color-text);
-		text-decoration: none;
-		padding: 1rem;
+	button {
+		all: unset;
 	}
 
 	.card-container {
@@ -79,10 +62,35 @@
 	.card {
 		background-color: var(--color-foreground);
 		border-radius: 10px;
-		padding: 20px;
-		margin: 20px;
+		margin: 0 16px;
+		padding: 1rem;
 		box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
 		width: 300px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		transition: transform 0.1s cubic-bezier(0.5, 0, 0.5, 1);
+	}
+	.card:hover {
+		transform: scale(1.05);
+	}
+	.card img {
+		transition: transform 0.1s cubic-bezier(0.5, 0, 0.5, 1),
+			filter 0.1s cubic-bezier(0.5, 0, 0.5, 1);
+	}
+	.card:hover img {
+		transform: translateY(-5px) scale(1.05);
+		filter: drop-shadow(0px 6px 2px rgb(0, 0, 0, 0.5));
+	}
+	.img-container {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+	}
+	.img-container img {
+		height: 50px;
+		filter: drop-shadow(0px 2px 2px rgb(0, 0, 0, 0.5));
 	}
 
 	.title {
@@ -115,5 +123,6 @@
 		justify-content: center;
 		align-items: center;
 		flex-grow: 1;
+		margin-bottom: 20%;
 	}
 </style>
---
+++ src/routes/projects/+layout.svelte
@@ -0,0 +1,28 @@
+<div class="navigation">
+	<a href="/">Home</a>
+	<a href="/projects"><img src="/img/projects_title.png" alt="Projects" draggable="false" /></a>
+	<a href="/about"><img src="/img/about_title.png" alt="About" draggable="false" /></a>
+	<a href="/contact"><img src="/img/contact_title.png" alt="Contact" draggable="false" /></a>
+</div>
+
+<slot />
+
+<style>
+	.navigation {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		display: flex;
+		background-color: var(--color-foreground);
+		box-shadow: 0 0px 10px rgba(0, 0, 0, 1);
+	}
+	.navigation a {
+		color: var(--color-text);
+		text-decoration: none;
+		padding: 1rem;
+	}
+	.navigation img {
+		height: 32px;
+	}
+</style>
